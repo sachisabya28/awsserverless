06b2eb30fa99a63a23590016ee9427518476e29c
sabya.sachi <sabya.sachi@rackspace.com>
2022-01-08 02:27:30 +0530

    JWT token added

--- auth/user.py
+++ auth/user.py
@@ -1,34 +1,15 @@
-# import os
-# import jwt
 import json
 import boto3
-# import base64
-from datetime import datetime, timedelta
+from boto3.dynamodb.types import Binary
+from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-# from io import BytesIO
-# import onetimepass
-# import pyqrcode
-# from boto3.dynamodb.conditions import Key, Attr
 
-
+from lib import auth
 '''
 create dynamodb resource
 '''
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 table = dynamodb.Table('dev-users')
-
-JWT_SECRET = 'secret'
-JWT_ALGORITHM = 'HS256'
-JWT_DELTA_SECONDS = 172800  # 2 days
-
-
-# def min_token(a_username):
-#     payload = {
-#         'username': a_username,
-#         'expiresIn': datetime.utcnow() + timedelta(seconds=JWT_DELTA_SECONDS)
-#     }
-#     jwt_token = jwt.encode(payload, JWT_SECRET, JWT_ALGORITHM)
-#     return jwt_token
 
 
 def get_user_by_username(username):
@@ -71,18 +52,8 @@ def build_response(statuscode, body=None):
     return response
 
 
-# def get_totp_uri(username):
-#     otp_secret = base64.b32encode(os.urandom(10)).decode('utf-8')
-#     return 'otpauth://totp/2FA-Demo:{0}?secret={1}&issuer=2FA-Demo' \
-#         .format(username, otp_secret)
-
-
 def verify_password(password):
     return check_password_hash(generate_password_hash(password), password)
-
-
-# def verify_totp(token, otp_secret):
-#     return onetimepass.valid_totp(token, otp_secret)
 
 
 def create_user(event, context):
@@ -119,39 +90,21 @@ def create_user(event, context):
         return build_response(422, body={
             'message': "Validation Failed, email used"
             })
-    '''
-    Implemented for 2FA
-    '''
-    # otp_secret = base64.b32encode(os.urandom(10)).decode('utf-8')
-    # otp = 'otpauth://totp/2FA-Demo:{0}?secret={1}&issuer=2FA-Demo' \
-    #     .format(data['username'], otp_secret)
-    # token = min_token(data['username'])
+    password_fields = auth.encode_password(data['password'])
     item = {
-        # 'id': str(uuid.uuid1()),
         'username': data['username'],
         'email': data['email'],
-        'password': generate_password_hash(data['password']),
-        # 'token': token,
         'createdAt': timestamp,
-        'updatedAt': timestamp
+        'hash': password_fields['hash'],
+        'salt': Binary(password_fields['salt']),
+        'rounds': password_fields['rounds'],
+        'hashed': Binary(password_fields['hashed'])
     }
     # create the user to the database
     table.put_item(Item=item)
-
-    # render qrcode for FreeTOTP
-    # url = pyqrcode.create(get_totp_uri(data['username']))
-    # stream = BytesIO()
-    # url.svg(stream, scale=3)
-    # return stream.getvalue(), 200, {
-    #     'Content-Type': 'image/svg+xml',
-    #     'Cache-Control': 'no-cache, no-store, must-revalidate',
-    #     'Pragma': 'no-cache',
-    #     'Expires': '0'}
-
     body = {
         'email': data['email'],
         'username': data['username']
-        # 'token': token
     }
     return build_response(200, body)
 
@@ -181,15 +134,12 @@ def login_user(event, context):
         return build_response(422, body={
             'message': "Validation Failed, inccorect password"
             })
-    # print(get_user_with_email['Items'][0]['username'])
-    # if not verify_totp(data['token'], get_user_with_email['Item'][0]
-    #                    ['otp_secret']):
-    #     raise Exception("Wrong token.", 422)
-
+    jwt_token = auth.get_jwt_token(
+        data['username'], data['password'], get_user_with_email['Items'][0])
+    
     authenticated_user = {
         'email': data['email'],
-        # 'token': min_token(get_user_with_email['Items'][0]['username']),
-        'username': get_user_with_email['Items'][0]['username']
+        'username': get_user_with_email['Items'][0]['username'],
+        'token': jwt_token
     }
-
     return build_response(200, authenticated_user)
